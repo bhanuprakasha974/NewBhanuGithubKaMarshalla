59b14fe30c817c342a1e30b4a27d5685a093ecbd
USER <USER@HP>
2022-09-28 11:00:19 +0530

    All is fine lets go on further.....

---
+++ src/test/java/CreaterTest/CreateOnce.java
@@ -0,0 +1,14 @@
+package CreaterTest;
+
+import org.testng.annotations.Test;
+
+public class CreateOnce {
+
+	@Test
+	public void create()
+	{
+		System.out.println("---created Once---");
+	}
+	
+	
+}
---
+++ src/test/java/CreaterTest/DeleteOnce.java
@@ -0,0 +1,15 @@
+package CreaterTest;
+
+import org.testng.annotations.Test;
+
+public class DeleteOnce {
+
+	
+
+	@Test
+	public void delete()
+	{
+		System.out.println("---deleted Once---");
+	}
+	
+}
